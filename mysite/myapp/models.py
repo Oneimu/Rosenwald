f69231024aa07e727b30d230aa3a725b1bcfd363
Miles College <hbcuc2-miles@miless-mbp.wireless.fisk.edu>
2021-10-04 22:25:47 -0500

    added the data to be sent to frontend

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+import json
 
-class
+class SchoolData:
+    # school_data : dict
+    def __init__(self):
+        # read file
+        my_json_file = open('/Users/hbcuc2-miles/rosenwald/Rosenwald/json_files/school_data.json', 'r')
+        json_data = my_json_file.read()
+
+        # parse data
+        self.school_data = json.loads(json_data)
 # Create your models here.
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import SchoolData
 
 
 def index(request):
-    return render(request, 'index.html')
+    schools = SchoolData()
+    data = schools.school_data
+    #change school name to a dict 
+    school_names = []
+    for school in data:
+        if data[school]["School Names"] not in school_names:
+            school_names.append(data[school]["School Names"])
+
+    return render(request, 'index.html', {'school_names': school_names})
 
 
-# Create your views here.
+# Create your views here
