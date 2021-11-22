c21bd6b58cd2e9b5fc0a861c426d4dbababe04cd
caowens <70489592+caowens@users.noreply.github.com>
2021-11-21 20:38:40 -0600

    home page has clickables

--- roseSite/models.py
+++ roseSite/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 import json
 
-class SchoolNames(models.Model):
+class Schools(models.Model):
+    #  mjf = my_json_file
+    #  Possibly make mjf and school_data private vars
+       mjf = open('/Users/alexowens/rosenwaldAlex3/Rosenwald/roseSite/static/js/school_data.json')
+       school_data = json.loads(mjf.read())
+
+class SchoolInfo(models.Model):
     school_name : str
-    # my_json_file = open('/Users/hbcuc2-miles/rosenwal/roseSite/static/js/school_data.json')
-    # school_data = json.loads(my_json_file.read())
+    photo_title : str
+    description : str
+    publisher : str
+
     # for names in school_data.values():
     #     self.objects.create(school_name=names['School Names'])
     # context = {'name': self.objects.all()}
 
     
 
-# Create your models here.
+# Create your models here.s
+
--- roseSite/views.py
+++ roseSite/views.py
@@ -1,23 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import SchoolNames
+from .models import SchoolInfo, Schools
 import json
 import random
 
+'''
+Eventually we are going to have to send the pictures to the front by 
+using the picture ID
+
+This code is a bit much but will be optimized once the frontend makes it convient for 
+the data to be pushed through a simple for loop
+
+The school names change everytime we refresh the page. 
+Should it be static or keep it as it is?
+
+'''
 def index(request):
-    schools1 = SchoolNames()
-    schools2 = SchoolNames()
-    schools3 = SchoolNames()
-    schools4 = SchoolNames()
-    schools5 = SchoolNames()
-    schools6 = SchoolNames()
-    my_json_file = open('/Users/hbcuc2-miles/rosenwal/roseSite/static/js/school_data.json')
-    school_data = json.loads(my_json_file.read())
+
+    schools = Schools().school_data
+    schools1 = SchoolInfo()
+    schools2 = SchoolInfo()
+    schools3 = SchoolInfo()
+    schools4 = SchoolInfo()
+    schools5 = SchoolInfo()
+    schools6 = SchoolInfo()
+
     i = 0
     s_names = set()
     # all unti
 
-    for names in school_data.values():
+    for names in schools.values():
         s_names.add(names['School Names'])
 
     for name in s_names:
@@ -43,5 +55,7 @@ def index(request):
     # context = {'name': school_names.objects.all()}
     return render(request, 'index.html', {'school1': schools1, 'school2': schools2, 'school3': schools3, 'school4': schools4, 'school5': schools5, 'school6': schools6})
 
+def results(request):
+    pass
 # Create your views here.
 
